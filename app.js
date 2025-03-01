a64ccf76f7dc31025b76a934fbff3ab3316e198e
Mahesh Thombare <maheshs.thombare@gmail.com>
2025-03-02 02:16:21 +0530

    env file changes

--- app.js
+++ app.js
@@ -114,31 +114,31 @@ app.get("/",(req,res)=>{
 
 
 //openai api
-// const openai = new OpenAI({
-//     apiKey: "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",
-//   });
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
   
   
-//    async function openAI(){
+   async function openAI(){
             
-//   const completion = await openai.chat.completions.create({
-//     model: "gpt-4o-mini",
-//     store: true,
-//     messages: [
-//         { "role": "system", "content": "Classify the given text as either 'Suspicious' or 'Non-Suspicious'. Only return one of these two words." },
-//         { "role": "user", "content": text }
-//     ],
+  const completion = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    store: true,
+    messages: [
+        { "role": "system", "content": "Classify the given text as either 'Suspicious' or 'Non-Suspicious'. Only return one of these two words." },
+        { "role": "user", "content": text }
+    ],
     
   
-//   });
-//    responce=completion.choices[0].message.content;
+  });
+   responce=completion.choices[0].message.content;
 
   
-//  console.log(responce);
+ console.log(responce);
 
 
 
-// }
+}
 
 
 // note:: Name Use Just remove comment
